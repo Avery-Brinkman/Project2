0a3fc05ca02a2c72185693363bd1008f4fbe91dd
Avery Brinkman <averybrinkman@gmail.com>
2023-03-30 17:16:20 -0400

    implemented messages

--- server/src/group.cpp
+++ server/src/group.cpp
@@ -15,6 +15,15 @@ void Group::removeUser(const std::string_view userName) {
   }
 }
 
+int Group::postMessage(const std::string_view name, const std::string_view subject, const std::string_view content) {
+  int msgId = m_messages.size();
+  Message newMessage = { msgId, name.data(), subject.data(), content.data() };
+  return msgId;
+}
+
+Message Group::getMessage(const int messageId) const {
+  return m_messages.at(messageId);
+}
 
 std::vector<std::string> Group::getUsers() const {
   return m_users;
